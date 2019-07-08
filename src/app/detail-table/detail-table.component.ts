58d9351e84f3f3e8d30cbffa35b7528c4070a9c6
sagilorenzo1996 <sagilorenzo1996@gmail.com>
2019-07-08 22:49:13 +0530

    sdsdssss

--- src/app/detail-table/detail-table.component.ts
+++ src/app/detail-table/detail-table.component.ts
@@ -1,23 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-detail-table',
   templateUrl: './detail-table.component.html',
   styleUrls: ['./detail-table.component.css']
 })
-export class DetailTableComponent implements OnInit {
+export class DetailTableComponent implements OnInit, OnChanges {
 
   @Input() sportCode: string;
   sportData: any;
   displayedColumns: string[] = ['matchName', 'totalMatches', 'remainingMatches', 'titans', 'trojans', 'spartans', 'vikings', 'viewBoard'];
 
   constructor() {
-    this.sportData = window['sportsFiesta'][this.sportCode];
-    console.log(this.sportData);
-
   }
 
   ngOnInit() {
+    this.sportData = window['sportsFiesta'][this.sportCode];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.ngOnInit();
   }
 
 }
--- src/app/sport-page/sport-page.component.ts
+++ src/app/sport-page/sport-page.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-sport-page',
   templateUrl: './sport-page.component.html',
   styleUrls: ['./sport-page.component.css']
 })
-export class SportPageComponent implements OnInit {
+export class SportPageComponent implements OnInit, OnChanges {
 
   @Input() sportCode: string;
 
@@ -14,6 +14,10 @@ export class SportPageComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.ngOnInit();
   }
 
 }
